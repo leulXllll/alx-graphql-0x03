2ad285015d1dd3ff96a6d94ae5bdf514aecb46a0
Leul <leulzewduchanie@gmail.com>
2025-07-01 11:52:37 +0300

    added sentry for error monitoring purpose

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactNode } from "react";
+import * as Sentry from '@sentry/react';
 
 interface State {
   hasError: boolean;
@@ -21,7 +22,7 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps , State> {
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    console.log({ error, errorInfo });
+    Sentry.captureException(error, { extra: errorInfo });
   }
 
   render() {
